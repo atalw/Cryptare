65bf56068c5d821378b879d3af47713698c390c5
Akshit Talwar <akshitalwar11@gmail.com>
2017-10-13 23:01:09 +0530

    Added function for cryptarelite

--- firebase/cryptare/functions/index.js
+++ firebase/cryptare/functions/index.js
@@ -13,7 +13,46 @@ exports.btcPriceNotification = functions.https.onRequest((req, res) => {
 	const ref = admin.database().ref();
 	const devices = []
 
-	ref.child('user_ids').once('value').then(snap => {
+	ref.child('user_ids').child('users').once('value').then(snap => {
+		snap.forEach(childSnap => {
+			const device = childSnap.val();
+			devices.push(device)
+		});
+		return devices;
+	}).then(devices => {
+
+		const payload = {
+			notification: {
+				body: 'Check out the Bitcoin Price! 😲',
+				sound: 'default',
+				badge: '1'
+			}
+		};
+
+		var options = {
+			priority: "high",
+			timeToLive: 60 * 60 * 24 * 7
+		};
+		return admin.messaging().sendToDevice(devices, payload, options).then(() => {
+			res.send("Successfully sent message");
+		}).catch(error => {
+			res.send(error);
+		});
+	});
+});
+
+exports.btcPriceNotificationLite = functions.https.onRequest((req, res) => {
+
+	const key = req.query.key;
+
+	if (key != functions.config().btcpricenotif.key) {
+		res.status(403).send("Wrong API key");
+		return;
+	}
+	const ref = admin.database().ref();
+	const devices = []
+
+	ref.child('user_ids_lite').child('users').once('value').then(snap => {
 		snap.forEach(childSnap => {
 			const device = childSnap.val();
 			devices.push(device)
